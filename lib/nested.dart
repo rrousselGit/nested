e07d0409b73883010e7a42dec5843eb7d8e67d76
Remi Rousselet <darky12s@gmail.com>
2019-07-01 18:00:16 +0200

    moar tests

---
+++ lib/nested.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/widgets.dart';
+
+class Nested extends SingleChildStatelessWidget {
+  const Nested({Key key, this.nested = const [], Widget child})
+      : super(key: key, child: child);
+
+  final List<SingleChildWidget> nested;
+
+  @override
+  Widget build(BuildContext context, {Widget child}) {
+    var tree = _NestedHook(
+      child: child,
+    );
+    for (final provider in nested.reversed) {
+      tree = _NestedHook(
+        child: provider,
+        nextChild: tree,
+      );
+    }
+    return tree;
+  }
+}
+
+class _NestedHook extends StatelessWidget {
+  const _NestedHook({Key key, this.nextChild, this.child})
+      : assert(child != null),
+        super(key: key);
+
+  final Widget child;
+  final Widget nextChild;
+
+  @override
+  _NestedHookElement createElement() => _NestedHookElement(this);
+
+  @override
+  Widget build(BuildContext context) => child;
+}
+
+class _NestedHookElement extends StatelessElement {
+  _NestedHookElement(_NestedHook widget) : super(widget);
+
+  @override
+  _NestedHook get widget => super.widget as _NestedHook;
+}
+
+abstract class SingleChildWidget implements Widget {
+  @override
+  SingleChildWidgetElement createElement();
+}
+
+mixin SingleChildWidgetElement on Element {
+  Widget _widget;
+
+  @override
+  void mount(Element parent, dynamic newSlot) {
+    if (parent is _NestedHookElement) {
+      _widget = parent.widget.nextChild;
+    }
+    super.mount(parent, newSlot);
+  }
+}
+
+abstract class SingleChildStatelessWidget extends StatelessWidget
+    implements SingleChildWidget {
+  const SingleChildStatelessWidget({Key key, Widget child})
+      : _child = child,
+        super(key: key);
+
+  final Widget _child;
+
+  @override
+  Widget build(BuildContext context, {Widget child});
+
+  @override
+  SingleChildStatelessElement createElement() =>
+      SingleChildStatelessElement(this);
+}
+
+class SingleChildStatelessElement extends StatelessElement
+    with SingleChildWidgetElement {
+  SingleChildStatelessElement(SingleChildStatelessWidget widget)
+      : super(widget);
+
+  @override
+  Widget build() {
+    return widget.build(this, child: _widget ?? widget._child);
+  }
+
+  @override
+  SingleChildStatelessWidget get widget =>
+      super.widget as SingleChildStatelessWidget;
+}
+
+class Adapter extends SingleChildStatelessWidget {
+  const Adapter({Key key, this.builder}) : super(key: key);
+
+  final Widget Function(BuildContext context, Widget child) builder;
+
+  @override
+  Widget build(BuildContext context, {Widget child}) {
+    return builder(context, child);
+  }
+}
--- test/nested_test.dart
+++ test/nested_test.dart
@@ -1,112 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart' hide TypeMatcher;
 import 'package:flutter_test/flutter_test.dart';
-
-class Nested extends Widget {
-  const Nested({Key key, this.nested = const [], this.child})
-      : assert(child != null),
-        super(key: key);
-
-  final List<SingleChildWidget> nested;
-  final Widget child;
-
-  @override
-  NestedElement createElement() => NestedElement(this);
-}
-
-class NestedElement extends ComponentElement {
-  NestedElement(Nested widget) : super(widget);
-
-  @override
-  Nested get widget => super.widget as Nested;
-
-  @override
-  Widget build() {
-    var tree = _NestedHook(
-      element: this,
-      child: widget.child,
-    );
-    for (final provider in widget.nested.reversed) {
-      tree = _NestedHook(
-        element: this,
-        child: provider,
-        nextChild: tree,
-      );
-    }
-    return tree;
-  }
-}
-
-class _NestedHook extends StatelessWidget {
-  const _NestedHook({Key key, this.element, this.nextChild, this.child})
-      : assert(child != null),
-        super(key: key);
-
-  final NestedElement element;
-  final Widget child;
-  final Widget nextChild;
-
-  @override
-  _NestedHookElement createElement() => _NestedHookElement(this);
-
-  @override
-  Widget build(BuildContext context) => child;
-}
-
-class _NestedHookElement extends StatelessElement {
-  _NestedHookElement(_NestedHook widget) : super(widget);
-
-  @override
-  _NestedHook get widget => super.widget as _NestedHook;
-}
-
-abstract class SingleChildWidget implements Widget {
-  @override
-  SingleChildWidgetElement createElement();
-}
-
-mixin SingleChildWidgetElement on Element {
-  Widget _widget;
-
-  @override
-  void mount(Element parent, dynamic newSlot) {
-    if (parent is _NestedHookElement) {
-      _widget = parent.widget.nextChild;
-    }
-    super.mount(parent, newSlot);
-  }
-}
-
-abstract class SingleChildStatelessWidget extends StatelessWidget
-    implements SingleChildWidget {
-  const SingleChildStatelessWidget({Key key, Widget child})
-      : _child = child,
-        super(key: key);
-
-  final Widget _child;
-
-  @override
-  Widget build(BuildContext context, {Widget child});
-
-  @override
-  SingleChildStatelessElement createElement() =>
-      SingleChildStatelessElement(this);
-}
-
-class SingleChildStatelessElement extends StatelessElement
-    with SingleChildWidgetElement {
-  SingleChildStatelessElement(SingleChildStatelessWidget widget)
-      : super(widget);
-
-  @override
-  Widget build() {
-    return widget.build(this, child: _widget ?? widget._child);
-  }
-
-  @override
-  SingleChildStatelessWidget get widget =>
-      super.widget as SingleChildStatelessWidget;
-}
+import 'package:matcher/matcher.dart';
+import 'package:nested/nested.dart';
 
 void main() {
   testWidgets('can be with just the child', (tester) async {
@@ -118,52 +13,111 @@ void main() {
   });
   testWidgets('something2', (tester) async {
     await tester.pumpWidget(const Nested(
-      nested: [Foo(height: 42)],
+      nested: [
+        Foo(height: 42),
+        Foo(height: 10),
+      ],
       child: Text('foo', textDirection: TextDirection.ltr),
     ));
 
     expect(find.text('foo'), findsOneWidget);
-    final box = find.byType(SizedBox).first.evaluate().first.widget as SizedBox;
-    expect(box.height, equals(42));
+    final box = find.byType(SizedBox).evaluate().toList();
+    expect(box.length, equals(2));
+    expect(
+      box[0].widget,
+      const TypeMatcher<SizedBox>().having((s) => s.height, 'height', 42),
+    );
+    expect(
+      box[1].widget,
+      const TypeMatcher<SizedBox>().having((s) => s.height, 'height', 10),
+    );
   });
 
-  testWidgets('standalone', (tester) async {
+  testWidgets('nested inside nested', (tester) async {
+    await tester.pumpWidget(const Nested(
+      nested: [
+        Foo(height: 0),
+        Nested(
+          nested: [
+            Foo(height: 1),
+            Foo(height: 2),
+          ],
+        ),
+        Foo(height: 3),
+      ],
+      child: Text('foo', textDirection: TextDirection.ltr),
+    ));
+
+    expect(find.text('foo'), findsOneWidget);
+    final box = find.byType(SizedBox).evaluate().toList();
+    expect(box.length, equals(4));
+
+    for (var i = 0; i < 4; i++) {
+      expect(
+        box[i].widget,
+        const TypeMatcher<SizedBox>().having((s) => s.height, 'height', i),
+      );
+    }
+  });
+
+  testWidgets('no unnecessary rebuild', (tester) async {
+    var buildCount = 0;
+    await tester.pumpWidget(Nested(nested: [
+      Foo(didBuild: (_, __) => buildCount++),
+    ], child: Container()));
+
+    expect(buildCount, equals(1));
+
+    await tester.pumpWidget(Nested(nested: [
+      Foo(didBuild: (_, __) => buildCount++),
+    ], child: Container()));
+
+    expect(buildCount, equals(2));
+  });
+
+  testWidgets('no unnecessary rebuild', (tester) async {
+    var buildCount = 0;
+    final child = Nested(
+      nested: [
+        Foo(didBuild: (_, __) => buildCount++),
+      ],
+      child: Container(),
+    );
+
+    await tester.pumpWidget(child);
+
+    expect(buildCount, equals(1));
+    await tester.pumpWidget(child);
+
+    expect(buildCount, equals(1));
+  });
+
+  testWidgets('SingleChildWidget can be used by itself', (tester) async {
     await tester.pumpWidget(const Foo(
       height: 42,
       child: Text('foo', textDirection: TextDirection.ltr),
     ));
 
     expect(find.text('foo'), findsOneWidget);
-    final box = find.byType(SizedBox).first.evaluate().first.widget as SizedBox;
-    expect(box.height, equals(42));
+    final box = find.byType(SizedBox).evaluate().single.widget;
+    expect(
+      box,
+      const TypeMatcher<SizedBox>().having((s) => s.height, 'height', 42),
+    );
   });
 }
 
 class Foo extends SingleChildStatelessWidget {
-  const Foo({Key key, this.height, Widget child})
+  const Foo({Key key, this.didBuild, this.height, Widget child})
       : super(key: key, child: child);
 
   final double height;
 
+  final void Function(BuildContext context, Widget child) didBuild;
+
   @override
   Widget build(BuildContext context, {Widget child}) {
+    didBuild?.call(context, child);
     return SizedBox(height: height, child: child);
   }
-}
-
-class Adapter extends SingleChildStatelessWidget {
-  const Adapter({Key key, this.builder}) : super(key: key);
-
-  final Widget Function(BuildContext context, Widget child) builder;
-
-  @override
-  Widget build(BuildContext context, {Widget child}) {
-    return builder(context, child);
-  }
-}
-
-void foo() {
-  Adapter(
-    builder: (_, child) => Container(child: child),
-  );
 }
